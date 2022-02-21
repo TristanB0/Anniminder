4c86dd3b2a88a3073123bb9356bc7ee1e2aa6337
Tristan B <tristan.bony@hotmail.fr>
2022-02-21 19:25:10 +0100

    Fixed .env

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import os
-#from dotenv import load_dotenv
+import sqlite3
+from dotenv import load_dotenv
+from datetime import date
 
-#load_dotenv()
+load_dotenv()
 token = os.getenv("TOKEN")
 
 #os.chdir("/home/tristan/Documents/Projects/Anniminder_Discord_bot/")
@@ -10,7 +12,14 @@ token = os.getenv("TOKEN")
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print("Logged on as {0.user}".format(self.user))
+	
+	async def on_message(self, message):
+		if message.author == self.author:
+			return
+		
+		if message.content.startswith("myCake is"):
+			await message.channel.send("Work still ongoing")
 
 client = MyClient()
 
-#client.run(token)
+client.run(token)
